e4c5d23209cdc9c8e79b06928c13dafaa37fc9b2
沛霖 霍 <apkqiu@qq.com>
2026-05-23 23:28:18 +0800

    更新 xml2element 函数以支持 expose_dict，修改 parse_xml 返回值，调整 test.xml 格式

--- parser.py
+++ parser.py
@@ -2,9 +2,10 @@ import tkinter as tk
 import xml.etree.ElementTree as XmlTree
 import components
 
-def xml2element(element: XmlTree.Element, parent_element=None):
+def xml2element(element: XmlTree.Element,expose_dict, parent_element=None):
     tag = element.tag
     factory = components.gallery.get_factory(tag)()
+    expose_name = element.attrib.pop("{extensions}expose", None)
     for key, value in element.attrib.items():
         factory.set_value(key.split("."), value)
     factory.set_parent(parent_element)
@@ -12,15 +13,20 @@ def xml2element(element: XmlTree.Element, parent_element=None):
         factory.set_innerText(element.text.strip())
     object = factory.create()
     for child in element:
-        element = xml2element(child, object)
+        element = xml2element(child, expose_dict, object)
         factory.add_child(element)
-    return object or factory.late_create()
-    
+    object = object or factory.late_create()
+    if expose_name:
+        expose_dict[expose_name] = object
+    return object
 
 def parse_xml(string):
+    expose_dict = {}
     root = XmlTree.fromstring(string)
-    root_object = xml2element(root)
-    return root_object
+    root_object = xml2element(root,expose_dict)
+    return root_object, expose_dict
 
 if __name__ == "__main__":
-    parse_xml(open("test.xml").read()).mainloop()
+    root, exposed = parse_xml(open("test.xml").read())
+    exposed["my_label"].configure(text="I Changed!")
+    root.mainloop()
